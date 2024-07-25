9f5a8a1f70c245211946809ca1aa8ae5ed519581
AmirHossein-Dashtban <amirhossein.namaghi@gmail.com>
2024-07-25 22:46:00 +0330

    Create "gramSchmidt.js" and add the "qrDecomposition" function

---
+++ logic/gramSchmidt.js
@@ -0,0 +1,29 @@
+import {
+	norm2,
+	orthogonalization,
+	product,
+	transpose,
+	unify,
+} from './matrixUtility.js';
+
+function qrDecomposition(A_) {
+	const A = A_;
+	const n = A.length;
+	let Q = transpose(orthogonalization(transpose(A)).map((v) => unify(v)));
+
+	let R = [];
+	for (let i = 0; i < n; i++) {
+		R[i] = [];
+		for (let j = 0; j < n; j++) {
+			if (j >= i)
+				R[i][j] = product(
+					[transpose(A)[j]],
+					transpose([transpose(Q)[i]])
+				)[0][0];
+			else R[i][j] = 0;
+		}
+	}
+	return [Q, R];
+}
+
+export { qrDecomposition };
